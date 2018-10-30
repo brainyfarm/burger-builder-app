180515c23e7f79a0e0d89b06b50ba64becc2cf0f
Olawale Akinseye <brainyfarm@gmail.com>
2018-10-30 16:34:58 +0100

    add burger control

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,38 @@ import React, { Component } from 'react'
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0,
+      }
+    }
+
+    this.addIngredientHandler = this.addIngredientHandler.bind(this);
+    this.removeIngredientHandler = this.removeIngredientHandler.bind(this);
+
+  }
+
+  addIngredientHandler (type) {
+
+  }
+
+  removeIngredientHandler (type) {
+
+  }
+
   render () {
     return (
       <Aux>
-        <Burger />
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls />
       </Aux>
     );
   }
