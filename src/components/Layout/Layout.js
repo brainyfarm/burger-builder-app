adb488b1aed6a1c263b123e1236901186036a65e
Olawale Akinseye <brainyfarm@gmail.com>
2018-11-08 09:04:55 +0100

    adjust sidedrawer in layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
@@ -6,15 +6,35 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
 
-const Layout = ( props ) => (
-    <Aux>
-      <Toolbar />
-      <SideDrawer />
-      <main className={classes.Content}>
-        {props.children}
-      </main>
-    </Aux>
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
 
-)
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => ({
+      showSideDrawer: !prevState.showSideDrawer
+    }));
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar 
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
